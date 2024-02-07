886970926501266fa760a5014f573ee13ee16986
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-02-07 16:36:24 +0300

    Restore an old example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,20 +1,10 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:banuba_sdk_example/page_camera.dart';
-import 'package:banuba_sdk_example/page_image.dart';
-import 'package:banuba_sdk_example/page_touchup.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:async';
 
-const banubaToken = <#"Place Token here"#>
-
-enum EntryPage { camera, image, touchUp }
+import 'package:banuba_sdk/banuba_sdk.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -25,116 +15,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  final _banubaSdkManager = BanubaSdkManager();
+  final _epWidget = EffectPlayerWidget(key: null);
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    await _banubaSdkManager.initialize([],
+        <#"Place Token here"#>,
+        SeverityLevel.info);
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {});
+    await _banubaSdkManager.openCamera();
+    await _banubaSdkManager.attachWidget(_epWidget.banubaId);
+
+    await _banubaSdkManager.startPlayer();
+    await _banubaSdkManager.loadEffect("effects/TrollGrandma", false);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final buttonStyle = ElevatedButton.styleFrom(
-      shape: const StadiumBorder(),
-      fixedSize: Size(MediaQuery.of(context).size.width / 2.0, 50),
-    );
-    Text textWidget(String text) {
-      return Text(
-        text.toUpperCase(),
-        style: const TextStyle(fontSize: 13.0),
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Face AR Flutter Sample'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            style: buttonStyle,
-            onPressed: () => _navigateToPage(EntryPage.camera),
-            child: textWidget('Open Camera'),
-          ),
-          SizedBox.fromSize(size: const Size.fromHeight(20.0)),
-          ElevatedButton(
-            style: buttonStyle,
-            onPressed: () => _navigateToPage(EntryPage.image),
-            child: textWidget('Image processing'),
-          ),
-          SizedBox.fromSize(size: const Size.fromHeight(20.0)),
-          ElevatedButton(
-            style: buttonStyle,
-            onPressed: () => _navigateToPage(EntryPage.touchUp),
-            child: textWidget('Touch Up features'),
-          ),
-        ],
-      ),
-    );
+    return MaterialApp(home: _epWidget);
   }
 
-  void _navigateToPage(EntryPage entryPage) {
-    switch (entryPage) {
-      case EntryPage.camera:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const CameraPage()),
-        );
-        return;
-
-      case EntryPage.image:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const ImagePage()),
-        );
-        return;
-
-      case EntryPage.touchUp:
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const TouchUpPage()),
-        );
-        return;
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _banubaSdkManager.startPlayer();
+    } else {
+      _banubaSdkManager.stopPlayer();
     }
   }
-}
-
-Future<String> generateFilePath(String prefix, String fileExt) async {
-  final directory = await getTemporaryDirectory();
-  final filename = '$prefix${DateTime.now().millisecondsSinceEpoch}$fileExt';
-  return '${directory.path}${Platform.pathSeparator}$filename';
-}
-
-// This is a sample implementation of requesting permissions.
-// It is expected that the user grants all permissions. This solution does not handle the case
-// when the user denies access or navigating the user to Settings for granting access.
-// Please implement better permissions handling in your project.
-Future<bool> requestPermissions() async {
-  final requiredPermissions = _getPlatformPermissions();
-  for (var permission in requiredPermissions) {
-    var ps = await permission.status;
-    if (!ps.isGranted) {
-      ps = await permission.request();
-      if (!ps.isGranted) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-List<Permission> _getPlatformPermissions() {
-  if (Platform.isAndroid) {
-    return [Permission.camera, Permission.microphone, Permission.storage];
-  } else if (Platform.isIOS) {
-    return [Permission.camera, Permission.microphone];
-  } else {
-    throw Exception('Platform is not supported!');
-  }
-}
-
-void showToastMessage(String message) {
-  Fluttertoast.showToast(
-      msg: message,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      timeInSecForIosWeb: 1,
-      textColor: Colors.white,
-      fontSize: 14.0
-  );
 }
