bac9710432455ee59ef4d96433ea527998ebb9a9
Gleb Prischepa <gleb.prischepa@banuba.com>
2024-02-17 16:26:07 +0300

    Update sample - request permissions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:async';
-
+import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:banuba_sdk/banuba_sdk.dart';
 
 void main() {
@@ -36,6 +38,25 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
     if (!mounted) return;
 
     setState(() {});
+
+    // It is required to grant all permissions for the plugin: Camera, Micro, Storage
+    requestPermissions().then((granted) {
+      if (granted) {
+        debugPrint('CameraPage: Thanks! All permissions are granted!');
+        openCamera();
+      } else {
+        debugPrint('CameraPage: WARNING! Not all required permissions are granted!');
+        // Plugin cannot be used. Handle this state on your app side
+        SystemNavigator.pop();
+      }
+    }).onError((error, stackTrace) {
+      debugPrint('CameraPage: ERROR! Plugin cannot be used : $error');
+      // Plugin cannot be used. Handle this state on your app side
+      SystemNavigator.pop();
+    });
+  }
+
+  Future<void> openCamera() async {
     await _banubaSdkManager.openCamera();
     await _banubaSdkManager.attachWidget(_epWidget.banubaId);
 
@@ -55,5 +76,33 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
     } else {
       _banubaSdkManager.stopPlayer();
     }
+  }
+}
+
+// This is a sample implementation of requesting permissions.
+// It is expected that the user grants all permissions. This solution does not handle the case
+// when the user denies access or navigating the user to Settings for granting access.
+// Please implement better permissions handling in your project.
+Future<bool> requestPermissions() async {
+  final requiredPermissions = _getPlatformPermissions();
+  for (var permission in requiredPermissions) {
+    var ps = await permission.status;
+    if (!ps.isGranted) {
+      ps = await permission.request();
+      if (!ps.isGranted) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+List<Permission> _getPlatformPermissions() {
+  if (Platform.isAndroid) {
+    return [Permission.camera, Permission.microphone, Permission.storage];
+  } else if (Platform.isIOS) {
+    return [Permission.camera, Permission.microphone];
+  } else {
+    throw Exception('Platform is not supported!');
   }
 }
